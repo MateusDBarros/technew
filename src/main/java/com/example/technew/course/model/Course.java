9fcde06d375cfcc5a3bc3539e0f13e091624c7ab
mateusDbarros <mb685212@gmail.com>
2025-01-14 02:56:52 -0300

    Commit

--- src/main/java/com/example/technew/course/model/Course.java
+++ src/main/java/com/example/technew/course/model/Course.java
@@ -1,4 +1,51 @@
 package com.example.technew.course.model;
 
 public class Course {
+
+    private int courseID;
+    private String name;
+    private String description;
+    private int personID;
+
+    public Course() {
+    }
+
+    public Course(int courseID, String name, String description, int personID) {
+        this.courseID = courseID;
+        this.name = name;
+        this.description = description;
+        this.personID = personID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
 }
--- src/main/java/com/example/technew/course/repository/CourseRepository.java
+++ src/main/java/com/example/technew/course/repository/CourseRepository.java
@@ -1,4 +1,22 @@
 package com.example.technew.course.repository;
 
+import com.example.technew.course.model.Course;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CourseRepository {
+
+    private final JdbcTemplate jdbc;
+
+
+    public CourseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void addNewCourse(Course course) {
+        String sql = "INSERT INTO Courses (name, description, personID) VALUES (?, ?, ?)";
+        jdbc.update(sql, course.getName(), course.getDescription(), course.getPersonID());
+    }
 }
